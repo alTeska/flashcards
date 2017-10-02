04322af28d7bfe32f40f733ef4e6876b911e8ccd
Aleksandra Teska <aleksandra.teska@gmail.com>
2017-10-02 16:51:28 +0200

    rename of file, Nom added

--- flashcards.py
+++ flashcards.py
@@ -13,6 +13,9 @@ def load_from_file(inp):
 def norm_word(word):
     return word.strip().lower()
 
+def norm_nom (word):
+    return word.strip()
+
 def make_sugest(word, i, ans):
     l = len(word)
     if not ans  : ans = list(l*"*")
@@ -24,9 +27,10 @@ def make_sugest(word, i, ans):
     ans[r] = word[r]
     return ans
 
-def find_match(inp, ans): #fix: down to letters
+def find_match(inp, ans):
     ratio, match_word = 0, ''
-    if ( isinstance(ans, str) ): ratio = fuzz.ratio(inp, ans)
+    if ( isinstance(ans, str) ):
+        return fuzz.ratio(inp, ans), ans
     else:
         for word in ans:
             temp = fuzz.ratio(inp, word)
@@ -37,42 +41,61 @@ class Verb(object):
     chances = 3
     def __init__(self, line):
         words = line.split()
-        self.en = words[1:]
         self.de = words[0]
+        self.en = words[1:]
 
-    def check_answer(self, inp, ans):
+    def check_ans(self, inp, ans):
         ratio, match_word = find_match(inp, ans)
         if ratio == 100:
             print('*correct*\n')
             self.chances = 0
-        elif ratio >= 90:
+        elif ratio >= 90: #dev
             print (match_word)
         else:
             print('*wrong*\n')
         self.chances -= 1
 
-    def play_round(self, way):
-        if   way == 0: translate, answer = self.de, self.en
-        else         : translate, answer = self.en[0], self.de
-
+    def transl_round(self, way):
+        if   way == 0: translate, ans = self.de, self.en
+        else         : translate, ans = self.en[0], self.de
         while self.chances > 0:
-            inp = norm_word( input(translate + " translate: ") ) #str matching
-            self.check_answer(inp, answer)
+            inp = norm_word( input(translate + " translate %s: " % way) )
+            self.check_ans(inp, ans)
 
+class Nom(Verb):
+    def __init__(self, line):
+        words = line.split()
+        self.art = words[0]
+        self.de  = words[1]
+        self.en  = words[2:]
+
+    def transl_round(self, way):
+        if   way == 0: translate, ans = self.de   , self.en
+        else         : translate, ans = self.en[0], (self.art + ' ' + self.de)
+        while self.chances > 0:
+            inp = norm_word( input(translate + " translate %s: " % way) )
+            print (inp, ans)
+            self.check_ans(inp, ans.lower())
 
 turns   = 1 #int( input("Amount of turns: ") )
 types   = 1
 points  = 0
 guessed = []
-
 transl_dir = 1
 
-if   (types == 1): dic = load_from_file( open('germ_eng.txt' , 'r') )
-elif (types == 2): dic = load_from_file( open('germ_noms.txt', 'r') )
+#if   (types == 1): dic = load_from_file( open('germ_eng.txt' , 'r') )
+#elif (types == 2): dic = load_from_file( open('germ_noms.txt', 'r') )
+dic_ver = load_from_file( open('germ_verb.txt', 'r') )
+dic_nom = load_from_file( open('germ_noms.txt', 'r') )
 
-for i in range(2):
+for i in range(turns):
+    #verb
     rand = random_pick(dic)
     word = Verb( dic[rand] )
-
-    word.play_round( transl_dir )
+    word.transl_round( transl_dir )
     guessed.append ( dic.pop(rand) )
+
+    #nomen
+    rand_nom = random_pick(dic_nom)
+    nom      = Nom( dic_nom[rand_nom] )
+    nom.transl_round( transl_dir )
