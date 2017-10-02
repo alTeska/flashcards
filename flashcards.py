9c456f687c290c93699b8c470001c370ca154ed3
Aleksandra Teska <aleksandra.teska@gmail.com>
2017-10-02 14:14:44 +0200

    todo moved out of file, both ge<->en dics working, clear up form gui options

--- flashcards.py
+++ flashcards.py
@@ -1,17 +1,3 @@
-'''word pairing German:English
-1) pop up or gui [PyQt]
-2) verbs, adrektives, noms #types = int( input("\n1.verben\n2.nomen\npick: ") )
-3) add a word by user
-4) once guessed removed -> should pop back? #if i > guessi: dic.append( guessed.pop(0) )
-5) mistake correction showing -> 1.fuzzy string matching, 2.additional requirement 3.show only after last try
-6) __str__ method for the class
-7) Eng <-> Germ #language = int( input( "1.German:English\n2.English:German\n3.Both\n" ) )
-
-Groups:
-    verbs - many answers, dat/akk choice - > Verb type: 1.Nom 2.Dat 3.Akk 4.Dat+Akk
-    noms  - der/die/das, plural
-    adjektive + special words
-'''
 from fuzzywuzzy import fuzz
 from random     import randint
 
@@ -27,13 +13,6 @@ def load_from_file(inp):
 def norm_word(word):
     return word.strip().lower()
 
-def find_match(tbl, inp):
-    ratio, match_word = 0, ''
-    for word in tbl:
-        temp = fuzz.ratio(inp, word)
-        if temp > ratio: ratio, match_word = temp, word
-    return ratio, match_word
-
 def make_sugest(word, i, ans):
     l = len(word)
     if not ans  : ans = list(l*"*")
@@ -45,50 +24,55 @@ def make_sugest(word, i, ans):
     ans[r] = word[r]
     return ans
 
-class Word:
+def find_match(inp, ans): #fix: down to letters
+    ratio, match_word = 0, ''
+    if ( isinstance(ans, str) ): ratio = fuzz.ratio(inp, ans)
+    else:
+        for word in ans:
+            temp = fuzz.ratio(inp, word)
+            if temp > ratio: ratio, match_word = temp, word
+    return ratio, match_word
+
+class Verb(object):
+    chances = 3
     def __init__(self, line):
         words = line.split()
         self.en = words[1:]
         self.de = words[0]
+
+    def check_answer(self, inp, ans):
+        ratio, match_word = find_match(inp, ans)
+        if ratio == 100:
+            print('*correct*\n')
+            self.chances = 0
+        elif ratio >= 90:
+            print (match_word)
+        else:
+            print('*wrong*\n')
+        self.chances -= 1
+
+    def play_round(self, way):
+        if   way == 0: translate, answer = self.de, self.en
+        else         : translate, answer = self.en[0], self.de
+
+        while self.chances > 0:
+            inp = norm_word( input(translate + " translate: ") ) #str matching
+            self.check_answer(inp, answer)
+
 
 turns   = 1 #int( input("Amount of turns: ") )
 types   = 1
 points  = 0
 guessed = []
 
+transl_dir = 1
+
 if   (types == 1): dic = load_from_file( open('germ_eng.txt' , 'r') )
 elif (types == 2): dic = load_from_file( open('germ_noms.txt', 'r') )
 
-for i in range(turns):
-    suggest = []
-    helps   = 0
-    guesses = 4
-    r    = random_pick(dic)
-    word = Word(dic[r])
+for i in range(2):
+    rand = random_pick(dic)
+    word = Verb( dic[rand] )
 
-    while guesses > 0:
-        answer = norm_word( input(word.de + " in English: ") )
-        #GUI options
-        if   (answer == '!'):
-            print ("*answer: " + word.de + '*\n')
-            break
-        elif (answer == '>'):
-            print('*word changed*\n')
-            break
-        elif (answer == '?'):
-            suggest = make_sugest(word.en[0], helps, suggest)
-            helps += 1
-            print('to '+''.join(suggest), '\n')
-
-        ratio, match_word = find_match(word.en, answer)
-        if ratio == 100:
-            guessed.append(dic.pop(r))
-            points += 1
-            print('*correct*\n','points:', points,'\n')
-            break
-        elif ratio >= 80: #after last round?, additional checks: lenght
-            print (match_word)
-        else:
-            guesses -= 1
-            points  -= 1
-            print('*wrong*\n')
+    word.play_round( transl_dir )
+    guessed.append ( dic.pop(rand) )
