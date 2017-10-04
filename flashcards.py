98b8bb268a36150afea727c183135c27a933df7b
Aleksandra Teska <aleksandra.teska@gmail.com>
2017-10-04 14:57:15 +0200

    make_suggest added for both, new way of txt files to be done, separated check for der/die/das needed, special for class

--- flashcards.py
+++ flashcards.py
@@ -13,25 +13,11 @@ def load_from_file(inp):
 def norm_word(word):
     return word.strip().lower()
 
-def make_sugest(word, i, ans):
-    l = len(word)
-    if not ans  : ans = list(l*"*")
-    if     i > 2: return "*help limit reached*\n"
-
-    if   (i == 0): r = 0
-    elif (i == 1): r =-1
-    else         : r = randint(1, l-2)
-    ans[r] = word[r]
-    return ans
-
 def find_match(inp, ans):
     ratio, match_word = 0, ''
-    if ( isinstance(ans, str) ):
-        return fuzz.ratio(inp, ans), ans
-    else:
-        for word in ans:
-            temp = fuzz.ratio(inp, word)
-            if temp > ratio: ratio, match_word = temp, word
+    for word in ans:
+        temp = fuzz.ratio(norm_word(inp), norm_word(word))
+        if temp > ratio: ratio, match_word = temp, word
     return ratio, match_word
 
 def check_ans(inp, ans, chances):
@@ -45,58 +31,72 @@ def check_ans(inp, ans, chances):
         print ('*wrong*\n')
     return chances - 1
 
-def translate(transl, ans, chances, way):
-    while chances > 0:
-        inp     = norm_word( input(transl + " translate %s: " % way) )
-        chances = check_ans(inp, ans, chances)
-
 class Verb(object):
-    chances = 3
+    chances  = 3
+    help_cnt = 0
+    suggest = []
     def __init__(self, line):
         words = line.split()
-        self.de = words[0]
+        self.de = [words[0]]
         self.en = words[1:]
 
-    def transl_round(self, way):
-        if   way == 0: transl, ans = self.de   , self.en
-        else         : transl, ans = self.en[0], self.de
-        translate(transl, ans, self.chances, way)
+    def pick_dir(self, way):
+        if   way == 0: self.tr, self.ans = self.de, self.en
+        else         : self.tr, self.ans = self.en, self.de
 
 class Nom(object):
-    chances = 3
+    chances  = 3
+    help_cnt = 0
+    suggest = []
     def __init__(self, line):
         words = line.split()
-        self.art = words[0]
-        self.de  = words[1]
+        self.art = [words[0]]
+        self.de  = [words[1]]
         self.en  = words[2:]
 
-    def transl_round(self, way):
-        if   way == 0: transl, ans = self.de   , self.en
-        else         : transl, ans = self.en[0], (self.art + ' ' + self.de)
-        translate(transl, ans, self.chances, way)
+    def pick_dir(self, way):
+        if   way == 0: self.tr, self.ans = self.de, self.en
+        else         : self.tr, self.ans = self.en, self.de
+
+def make_suggest(ans, i, suggest):
+    l = len(ans)
+    if not suggest: suggest = list(l*"*")
+    if     i > 2  : return "*help limit reached*\n"
+
+    if   (i == 0): r = 0
+    elif (i == 1): r =-1
+    else         : r = randint(1, l-2)
+    suggest[r] = ans[r]
+    return suggest
 
 def game_round(dic, Word, transl_dir):
     rand = random_pick(dic)
     word = Word( dic[rand] )
-    word.transl_round( transl_dir )
+    word.pick_dir( transl_dir )
+
+    while word.chances > 0:
+        #print(word.tr)
+        #print(word.ans)
+        inp = input(word.tr[0] + " translate %s: " % transl_dir)
+        if (inp == '?'): #GUI options
+            word.suggest = make_suggest(word.ans[0], word.help_cnt, word.suggest)
+            word.help_cnt += 1
+            print(''.join(word.suggest), '\n')
+        else:
+            word.chances = check_ans(inp, word.ans, word.chances)
+            if (transl_dir == 1 and types ==1 ): print ('a')
     dic.pop( rand )
 
-turns      = 1   #int( input("Amount of turns: ") )s
+turns      = 1   #int( input("Amount of turns: ") )
 types      = 1   #int( input("\n1.verben\n2.nomen\npick: ") )
 points     = 0
-transl_dir = 0   #int( input("1.German:English\n2.English:German\n3.Both\n") )
+transl_dir = 1   #int( input("1.German:English\n2.English:German\n3.Both\n") )
 guessed    = []  #guessed.append ( dic_ver.pop(rand_ver) )
 
 dic_ver = load_from_file( open('germ_verb.txt', 'r') )
 dic_nom = load_from_file( open('germ_noms.txt', 'r') )
-
 if   (types == 0): dic, Word = dic_ver, Verb     #verb
 elif (types == 1): dic, Word = dic_nom, Nom      #nomen
 
 for i in range(turns):
-    #GUI options
-    #answer = norm_word( input(word.de + " in English: ") )
-    #if (answer == '?'):
-    #    suggest = make_sugest(word.en[0], helps, suggest)
-
     game_round(dic, Word, transl_dir)
